89fb968ad598884bfe0ff72fe73b0e89d57c77da
OceaneCsn <oceane.cassan@insa-lyon.fr>
2018-10-05 21:02:08 +0200

    GUI+comm
    Error in the CFP answers reception
    (receives the responses from the first CFP over and over again)

--- Covoit/src/CovoitClasses/CovoitAgent.java
+++ Covoit/src/CovoitClasses/CovoitAgent.java
@@ -1,6 +1,8 @@
 package CovoitClasses;
 import java.util.*;
 
+
+
 import jade.core.AID;
 import jade.core.Agent;
 import jade.core.behaviours.*;
@@ -14,7 +16,7 @@ import jade.domain.FIPAAgentManagement.ServiceDescription;
 
 public class CovoitAgent extends Agent {
 	
-	//private CovoitGui mygui;
+	private CovoitAgentGui myGui;
 	
 	private String startingCity;
 	private String targetCity;
@@ -25,18 +27,24 @@ public class CovoitAgent extends Agent {
 	private ArrayList<AID> refused;
 	private int price;
 	private ArrayList<AID> acquaintances;
+	private Boolean recruited;
 	
-	
-	protected void setup( ) {
+	protected void setup() {
+		myGui = new CovoitAgentGui(this);
+		myGui.showGui();
 		
-		startingCity = "Montpellier";
+	}
+	
+	protected void init() {		
+		/*startingCity = "Montpellier";
 		targetCity = "Lyon";
 		leavingTime = 15;
 		carScore = 4;
 		nbPlaces = 3;
-		price = 4; 
+		price = 4; */
 		passengers = new ArrayList<AID>();
 		refused = new ArrayList<AID>();
+		recruited = false;
 		
 		// Register the book-selling service in the yellow pages
 		DFAgentDescription dfd = new DFAgentDescription();
@@ -54,6 +62,7 @@ public class CovoitAgent extends Agent {
 		
 		addBehaviour(new NegociationDriver());
 		addBehaviour(new NegociationPassenger());
+		addBehaviour(new PleaseDie());
 	}
 	
 	private class NegociationDriver extends CyclicBehaviour {
@@ -61,108 +70,130 @@ public class CovoitAgent extends Agent {
 		private MessageTemplate mt;
 		
 		public void action(){
-			DFAgentDescription template = new DFAgentDescription();
-			ServiceDescription sd = new ServiceDescription();
-			sd.setType(startingCity+";"+targetCity);
-			template.addServices(sd);
-			try {
-				DFAgentDescription[] result = DFService.search(myAgent, template); 
-				//System.out.println("Found the following agents:");
-				acquaintances = new ArrayList(result.length);
-				for (int i = 0; i < result.length; ++i) {
-					acquaintances.add(result[i].getName());
-					//System.out.println(result[i].getName());
-				}
-				acquaintances.remove(getAID());
-				for(AID a:passengers) {
-					acquaintances.remove(a);
-				}
-				for(AID a:refused) {
-					acquaintances.remove(a);
-				}
-			}
-			catch (FIPAException fe) {
-				fe.printStackTrace();
+			
+			if(recruited) {
+				System.out.println(getAID().getName()+" recruited at the beginning of the loop");
 			}
 			
-			
-			//envoi aux premier passager potentiel
-			ACLMessage cfp = new ACLMessage(ACLMessage.CFP);
-			
-			for (int i = 0; i < acquaintances.size(); ++i) {
-				cfp.addReceiver(acquaintances.get(i));
-			} 
-			cfp.setContent(String.valueOf(price));
-			cfp.setConversationId("covoit");
-			cfp.setReplyWith("cfp"+System.currentTimeMillis()); // Unique value
-			myAgent.send(cfp);
-			
-			
-			// Prepare the template to get proposals
-			mt = MessageTemplate.MatchConversationId("cfp_answer");
-			//System.out.println("template matched");
-			ACLMessage reply = myAgent.receive(mt);
-			//System.out.println("message received");
-			if(reply != null) {
-				System.out.println("message received");
-				if(reply.getPerformative()== ACLMessage.PROPOSE) {
-					System.out.println("cfp proposed");
-					
-					passengers.add(reply.getSender());
-					nbPlaces --;
-					if(nbPlaces == 0){
-						for(AID a : passengers) {
-							ACLMessage die = new ACLMessage(ACLMessage.REQUEST);
-							die.addReceiver(a);
-							die.setConversationId("apoptosis");
-							myAgent.send(die);
-						}
-						doDelete();
-						
+			if(!recruited) {
+				DFAgentDescription template = new DFAgentDescription();
+				ServiceDescription sd = new ServiceDescription();
+				sd.setType(startingCity+";"+targetCity);
+				template.addServices(sd);
+				try {
+					DFAgentDescription[] result = DFService.search(myAgent, template); 
+					//System.out.println("Found the following agents:");
+					acquaintances = new ArrayList(result.length);
+					for (int i = 0; i < result.length; ++i) {
+						acquaintances.add(result[i].getName());
+						//System.out.println(result[i].getName());
 					}
-					
-					
-					ACLMessage confirm = new ACLMessage(ACLMessage.ACCEPT_PROPOSAL);
-					confirm.addReceiver(reply.getSender());
-					confirm.setContent(String.valueOf(price/(1+acquaintances.size())));
-					confirm.setConversationId("covoit");
-					myAgent.send(confirm);
-					System.out.println(getAID().getName()+" accepted proposal from"+reply.getSender());
-					System.out.println(String.valueOf(passengers.size()));
-					System.out.println(String.valueOf(nbPlaces));
+					acquaintances.remove(getAID());
+					for(AID a:passengers) {
+						System.out.println(a+" removed");
+						acquaintances.remove(a);
+					}
+					for(AID a:refused) {
+						acquaintances.remove(a);
+					}
+				}
+				catch (FIPAException fe) {
+					fe.printStackTrace();
 				}
 				
-				if(reply.getPerformative()== ACLMessage.REFUSE){
-					System.out.println(getAID().getName()+" refused proposal");
+				
+				//envoi aux premier passager potentiel
+				ACLMessage cfp = new ACLMessage(ACLMessage.CFP);
+				
+				for (int i = 0; i < acquaintances.size(); ++i) {
+					cfp.addReceiver(acquaintances.get(i));
+					System.out.println(getAID().getName()+"sending cfp to "+acquaintances.get(i).getName());
+				} 
+				cfp.setContent(String.valueOf(price));
+				cfp.setConversationId("covoit_cfp");
+				cfp.setReplyWith("cfp"+System.currentTimeMillis()); // Unique value
+				myAgent.send(cfp);
+				System.out.println(cfp.getReplyWith());
+				//mt = MessageTemplate.and(MessageTemplate.MatchConversationId("covoit_cfp"),
+				//		MessageTemplate.MatchInReplyTo(cfp.getReplyWith()));
+				//Prepare the template to get proposals
+				mt = MessageTemplate.MatchConversationId("covoit_cfp");
+				//mt = MessageTemplate.MatchInReplyTo(cfp.getReplyWith());
+				ACLMessage reply = myAgent.receive(mt);
+				if(reply != null) {
+					if(reply.getPerformative()== ACLMessage.PROPOSE) {
+						System.out.println("cfp proposal");
+						passengers.add(reply.getSender());
+						nbPlaces --;
+											
+						ACLMessage confirm = new ACLMessage(ACLMessage.ACCEPT_PROPOSAL);
+						confirm.addReceiver(reply.getSender());
+						confirm.setContent(String.valueOf(price/(1+acquaintances.size())));
+						confirm.setConversationId("covoit");
+						myAgent.send(confirm);
+						System.out.println(getAID().getName()+" accepted proposal from"+reply.getSender().getName());
+						System.out.println(String.valueOf(passengers.size()));
+						System.out.println(String.valueOf(nbPlaces));
+						
+						if(nbPlaces == 0){
+							for(AID a : passengers) {
+								ACLMessage die = new ACLMessage(ACLMessage.REQUEST);
+								die.addReceiver(a);
+								die.setConversationId("apoptosis");
+								myAgent.send(die);
+							}
+							doDelete();
+						}
+					}
 					
+					if(reply.getPerformative()== ACLMessage.REFUSE){
+						System.out.println(getAID().getName()+" refused proposal");
+					}
 				}
 			}
-			
+			try{
+				Thread.sleep(1000);
+				//System.out.println("pause");
+			}
+			catch(InterruptedException e){}
 		}
 	}
 	
 	private class NegociationPassenger extends CyclicBehaviour {
 		public void action(){
-			if(passengers.size() == 0) {
+			if(passengers.size() == 0 && !recruited) {
 				MessageTemplate mt = MessageTemplate.MatchPerformative(ACLMessage.CFP);
 				ACLMessage msg = myAgent.receive(mt);
 				if(msg != null) {
+					ACLMessage proposal = msg.createReply();
 					// if the proposed price is inferior than the the price of the agent's travel on its own
 					if(Integer.parseInt(msg.getContent()) <= price) {
 						//System.out.println("received price interesting");
-						ACLMessage proposal = new ACLMessage(ACLMessage.PROPOSE);
+						proposal.setPerformative(ACLMessage.PROPOSE);
 						proposal.addReceiver(msg.getSender());
 						proposal.setContent("ok");
-						proposal.setConversationId("cfp_answer");
-						myAgent.send(proposal);
-						//System.out.println(proposal.getSender()+" sent a proposal");
+						proposal.setConversationId("covoit_cfp");
+						System.out.println(getAID().getName()+" sent a proposal");
 					}
 					else {
-						ACLMessage refuse = new ACLMessage(ACLMessage.REFUSE);
-						myAgent.send(refuse);
+						
+						proposal.setPerformative(ACLMessage.REFUSE);
 					}
+					System.out.println(proposal.getInReplyTo());
+					myAgent.send(proposal);
 				}
-			}			
+				MessageTemplate mt2 = MessageTemplate.MatchPerformative(ACLMessage.ACCEPT_PROPOSAL);
+				ACLMessage msg2 = myAgent.receive(mt2);
+				if(msg2 != null) {
+					System.out.println(getAID().getName()+" recruited");
+					recruited = true;
+				}
+			}
+			try{
+				Thread.sleep(2000);
+				//System.out.println("pause");
+			}
+			catch(InterruptedException e){}
 		}
 	}
 	
@@ -176,20 +207,32 @@ public class CovoitAgent extends Agent {
 			fe.printStackTrace();
 		}
 		// Close the GUI
-		//myGui.dispose();
+		myGui.dispose();
 		// Printout a dismissal message
 		System.out.println("Agent "+getAID().getName()+" terminating.");
 	}
 	
-	protected class pleaseDie extends CyclicBehaviour{
+	protected void updateTravel(String sCity, String tCity, Integer lTime, Integer cScore, Integer nbP, Integer pr) {
+		startingCity = sCity;
+		targetCity = tCity;
+		leavingTime = lTime;
+		carScore = cScore;
+		nbPlaces = nbP;
+		price = pr;
+		System.out.println("Agent "+getAID().getName()+" going from "+startingCity+" to "+targetCity);
+		this.init();
+	}
+	
+	protected class PleaseDie extends CyclicBehaviour{
 		public void action(){
 			MessageTemplate mt = MessageTemplate.MatchPerformative(ACLMessage.REQUEST);
 			ACLMessage msg = myAgent.receive(mt);
-			if(msg.getConversationId().equals("apoptosis")) {
-				System.out.println("Agent "+getAID().getName()+" terminating.");
-				doDelete();
+			if(msg != null) {
+				if(msg.getConversationId().equals("apoptosis")) {
+					System.out.println("Agent "+getAID().getName()+" terminating.");
+					doDelete();
+				}
 			}
-			
 		}
 	}
 	
